686e60d6560144d518b60cf8d4caf9f5a6d5b63a
Jay <jpca999@gmail.com>
2022-01-10 13:55:24 -0500

    -

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,3 +1,4 @@
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../../styles/Home.module.css'
@@ -5,7 +6,7 @@ import styles from '../../../styles/Home.module.css'
 export default function ClientProjects() {
   return (
     <div className={styles.container}>
-      <h1> All projects of a given  Client</h1>
+      <h1> All projects of a given  Client  </h1>
 
     </div>
   )
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,12 +1,30 @@
+import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 
 export default function Clients() {
+
+  const clients = [
+    {id: 'max', name: 'MAx Million'}, 
+    {id: 'Amanda', name: 'Amanda Million'}, 
+    {id: 'Romeo', name: 'Romeo Million'}, 
+  ];
   return (
     <div className={styles.container}>
       <h1> Clients Page</h1>
-
+    {clients.map( (client) => (
+        <li key={client.id}> 
+        
+        <Link href={`/clients/${client.id}`}> {client.name}  </Link>  
+        
+         </li>
+      ))}
+      
+      <hr/>
+      <Link href="/clients/Max"> Max  </Link>
+      <Link href="/Romeo"> Romeo  </Link>
+      <Link href="/Amanda"> Amanda  </Link>
     </div>
   )
 }
