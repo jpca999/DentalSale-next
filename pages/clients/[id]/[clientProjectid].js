8cb57fee37374a3262d520b7d1f97091ce60f8b2
Jay <jpca999@gmail.com>
2022-01-10 12:47:27 -0500

    -

--- pages/clients/[id]/[clientProjectid].js
+++ pages/clients/[id]/[clientProjectid].js
@@ -1,11 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../../styles/Home.module.css'
+import  { useRouter } from 'next/router';
 
 export default function SingleIndividualProject() {
+
+    const router = useRouter(); 
+
+    console.log( ' router.query =', router.query );
+
+
   return (
     <div className={styles.container}>
-      <h1>  Single Individual project of a selected client </h1>
+      <h1>  Single Individual project of a selected client  {router.query.id} </h1>
+
+      <h1>  Single Individual project of a selected client  {router.query.clientProjectid} </h1>
+      
 
     </div>
   )
