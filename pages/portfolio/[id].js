3c900043e1d1e34bb65ae27075f0c396d4701064
Jay <jpca999@gmail.com>
2022-01-10 11:50:27 -0500

    query being passed to the single componnent

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -2,7 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 
+import { useRouter, withRouter } from 'next/router';
+
+
 export default function SinglePortfolioPage() {
+    const router = useRouter ();
+
+    console.log('11- router =', router );
+    console.log('11- router query =', router.query  );
+
   return (
     <div className={styles.container}>
       <h1> Single  Portfolio Page </h1>
