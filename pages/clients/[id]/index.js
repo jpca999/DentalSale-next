e52d451f7427b3c5c69441a6deb3c9f1884c3159
Jay <jpca999@gmail.com>
2022-01-10 14:34:28 -0500

    -

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -2,12 +2,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../../styles/Home.module.css'
+import { useRouter } from 'next/router'
 
 export default function ClientProjects() {
+  const router = useRouter(); 
+
+    console.log(' router.query '); 
+
+    function loadProjectHandler(){
+      console.log(' clicked -  loadProjectHandler');
+    }
+
   return (
     <div className={styles.container}>
       <h1> All projects of a given  Client  </h1>
 
+      <button onClick={loadProjectHandler}>Load a project  A </button>
     </div>
   )
 }
