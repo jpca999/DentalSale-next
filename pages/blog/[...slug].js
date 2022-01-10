6ca97a6fdb789669c6eda9f09660102faa364022
Jay <jpca999@gmail.com>
2022-01-10 13:27:44 -0500

    all routes catcher

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+import { useRouter, withRouter } from 'next/router';
+
+
+export default function BlogPostsPage() {
+    const router = useRouter ();
+
+    console.log('11- router =', router );
+    console.log('11- router query =', router.query  );
+
+  return (
+    <div className={styles.container}>
+      <h1> Single Blog  Page  All routes Catcher  like - 3000/blog/23/234234  {router.query.id} </h1>
+
+    </div>
+  )
+}
