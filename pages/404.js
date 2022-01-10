d062ca61aabc0fa3624c1cc5ad7582bc7d21edcd
Jay <jpca999@gmail.com>
2022-01-10 14:38:54 -0500

    -

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function NotFound() {
+  return (
+    <div className={styles.container}>
+      <h1> 404 - Dental Sale | Page not found </h1>
+    </div>
+  )
+}
