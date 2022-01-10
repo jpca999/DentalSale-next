172f37f1697dd04bbeafe2901cdff93412e505a9
Jay <jpca999@gmail.com>
2022-01-10 14:23:14 -0500

    -

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -6,21 +6,25 @@ import styles from '../../styles/Home.module.css'
 export default function Clients() {
 
   const clients = [
-    {id: 'max', name: 'MAx Million'}, 
-    {id: 'Amanda', name: 'Amanda Million'}, 
-    {id: 'Romeo', name: 'Romeo Million'}, 
+    {id: "max", name: "MAx Million"}, 
+    {id: "Amanda", name: "Amanda Million"}, 
+    {id: "Romeo", name: "Romeo Million"}
   ];
+
   return (
     <div className={styles.container}>
       <h1> Clients Page</h1>
-    {clients.map( (client) => (
-        <li key={client.id}> 
-        
-        <Link href={`/clients/${client.id}`}> {client.name}  </Link>  
-        
-         </li>
-      ))}
-      
+      <ul>
+        {clients.map( (client) => (
+            <li key={client.id}> 
+              {/* <p> {client.name} </p> */}
+            {/* <Link href="/">   <a>  </a> {client.name}  </Link>   */}
+
+            <Link href="/"><a> {client.name}  </a></Link>
+            
+            </li>
+          ))}
+      </ul>
       <hr/>
       <Link href="/clients/Max"> Max  </Link>
       <Link href="/Romeo"> Romeo  </Link>
