5946a8215ed469c151e0090b764fa70072ec83b1
Jay <jpca999@gmail.com>
2022-01-11 00:58:33 -0500

    -

---
+++ pages/events/[eventId].js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../../styles/Home.module.css'
+import  { useRouter } from 'next/router';
+
+export default function EventDetailPage () {
+
+    const router = useRouter(); 
+
+    console.log( ' router.query =', router.query );
+
+
+  return (
+    <div className={styles.container}>
+      <h1>  EventDetailPage  project of a selected client  {router.query.id} </h1>
+
+     
+      
+
+    </div>
+  )
+}
