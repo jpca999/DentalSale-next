5514f8c186e4b10a40e352295860c240c931600f
Jay <jpca999@gmail.com>
2022-01-11 11:44:07 -0500

    179- dummy  posts showing up

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,43 @@
-import Link from 'next/link'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { Fragment } from 'react'
-import Hero from '../components/home-page/Hero'
+import { Fragment } from 'react';
+import Head from 'next/head';
 
-export default function Home() {
+import FeaturedPosts from '../components/home-page/featured-posts';
+import Hero from '../components/home-page/hero';
+// import { getFeaturedPosts } from '../lib/posts-util';
 
+const DUMMY_POSTS = [
+  {slug: 'getting-started', title: 'the title', image: 'getting-started-with-nextjs.png', excerpt:'234', date: ' 2022-02-10' },
+  {slug: 'getting-started', title: 'the title', image: 'getting-started-with-nextjs.png', excerpt:'234', date: ' 2022-02-10' },
+  {slug: 'getting-started', title: 'the title', image: 'getting-started-with-nextjs.png', excerpt:'234', date: ' 2022-02-10' },
+  {slug: 'getting-started', title: 'the title', image: 'getting-started-with-nextjs.png', excerpt:'234', date: ' 2022-02-10' },
+  {slug: 'getting-started', title: 'the title', image: 'getting-started-with-nextjs.png', excerpt:'234', date: ' 2022-02-10' }
+];
 
+function HomePage(props) {
   return (
-     <Fragment>
-       <Hero />
-     </Fragment>
-
-  )
+    <Fragment>
+      <Head>
+        <title>Jay' Blog</title>
+        <meta
+          name='description'
+          content='I post about programming and web development.'
+        />
+      </Head>
+      <Hero />
+      {/* <FeaturedPosts posts={props.posts} /> */}
+      <FeaturedPosts posts={DUMMY_POSTS} />
+    </Fragment>
+  );
 }
+
+export function getStaticProps() {
+  // const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      // posts: featuredPosts,
+    },
+  };
+}
+
+export default HomePage;
