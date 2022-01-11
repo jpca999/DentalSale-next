19d5e4a90b7508f2f11009f06a68d2fdd1a98c66
Jay <jpca999@gmail.com>
2022-01-11 11:34:44 -0500

    177 done

--- pages/contact.js
+++ pages/contact.js
@@ -2,11 +2,11 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { getFeaturedEvents } from './dummyData'; 
+// import { getFeaturedEvents } from './dummyData'; 
 
 export default function Home() {
 
-  const FeaturedEvents = getFeaturedEvents(); 
+  // const FeaturedEvents = getFeaturedEvents(); 
 
   return (
     <div className={styles.container}>
