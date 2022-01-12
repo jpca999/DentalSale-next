5928a02544961517834caacff99678513fe1b234
Jay <jpca999@gmail.com>
2022-01-12 14:42:17 -0500

    -

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: 'jpca999',
+        mongodb_password: 'Jallishit9$',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: 'my-site-dev',
+      },
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: 'jpca999',
+      mongodb_password: 'Jallishit9$',
+      mongodb_clustername: 'cluster0',
+      mongodb_database: 'my-site',
+    },
+  };
+};
